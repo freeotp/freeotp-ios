7d34e45add8c9f0f7ad950ce3c055274140c0dcc
Nathaniel McCallum <npmccallum@redhat.com>
2013-10-21 17:18:20 -0400

    Relicense to Apache 2.0

--- FreeOTP/AddTokenViewController.m
+++ FreeOTP/AddTokenViewController.m
@@ -1,10 +1,23 @@
 //
-//  AddTokenViewController.m
-//  FreeOTP
+// FreeOTP
 //
-//  Created by Nathaniel McCallum on 10/11/13.
-//  Copyright (c) 2013 Nathaniel McCallum. All rights reserved.
+// Authors: Nathaniel McCallum <npmccallum@redhat.com>
 //
+// Copyright (C) 2013  Nathaniel McCallum, Red Hat
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 #import "AddTokenViewController.h"
 #import "Token.h"
 #import <float.h>
